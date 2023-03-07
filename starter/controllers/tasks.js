0a1d33487f08bcafb5f744e995c87e59edcf2be2
Andrew Kyalo Nzioki <andrewkyalonzioki001@gmail.com>
2023-03-08 01:42:10 +0300

    getting a single item

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,15 +1,39 @@
 const Task = require("../models/Task");
+
+//Get all Tasks
 const getAllTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.status(200).send(tasks);
+  try {
+    const tasks = await Task.find();
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+//Create one Task
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+
+//Get a single Task
 const getTask = async (req, res) => {
-  res.json({ id: req.params.id });
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID }).exec();
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskID}` });
+    }
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+
+//update a single Task
 const updateTask = (req, res) => {
   res.json({ id: req.params.id });
 };
--- starter/models/Task.js
+++ starter/models/Task.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must provide name"],
+    trim: true,
+    minlength: [2, "Name must be more than, 2 characters"],
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
+
 module.exports = mongoose.model("Task", TaskSchema);
